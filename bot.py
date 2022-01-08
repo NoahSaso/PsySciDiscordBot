5a6499a9759e7c3f3206f3cdbeaee7074afb420d
Noah Saso <noahsaso@berkeley.edu>
2022-01-07 18:04:25 -0800

    added command to refresh website (only board members can run it)

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ from disnake.ext import commands
 
 import os
 import sys
+import subprocess
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -15,7 +16,15 @@ ROLE_ID = int(os.getenv('ROLE_ID'))
 
 ANONYMOUS_MSGS_CHANNEL_ID = int(os.getenv('ANONYMOUS_MSGS_CHANNEL_ID'))
 
+ADMIN_ROLE_ID = int(os.getenv('ADMIN_ROLE_ID'))
+
 bot = commands.Bot(test_guilds=[GUILD_ID])
+
+async def check_admin_and_respond_if_not(inter):
+    if not inter.author.get_role(ADMIN_ROLE_ID):
+        await inter.response.send_message("You do not have permission to run this command.")
+        return False
+    return True
 
 @bot.event
 async def on_ready():
@@ -40,7 +49,7 @@ async def on_raw_reaction_add(payload):
 @bot.slash_command()
 async def server(inter):
     """
-    Get server stats
+    Get server stats.
     """
 
     await inter.response.send_message(
@@ -50,7 +59,7 @@ async def server(inter):
 @bot.slash_command()
 async def anonymous(inter, message):
     """
-    Send an anonymous message to the board
+    Send an anonymous message to the board.
 
     Parameters
     ----------
@@ -63,7 +72,32 @@ async def anonymous(inter, message):
     await inter.delete_original_message()
     await inter.author.send("Your anonymous message has been sent to the board.")
 
-    print("Sent anonymous message.")
+    print("Sent anonymous message")
+    sys.stdout.flush()
+
+@bot.slash_command()
+async def refresh_website(inter):
+    """
+    Update the website with the latest changes (must be a board member to activate).
+    """
+
+    if not await check_admin_and_respond_if_not(inter):
+        return
+
+    print("Refreshing website...")
+    sys.stdout.flush()
+
+    await inter.response.send_message("Refreshing website from Notion...")
+    
+    response = subprocess.run(["/home/p/ps/psab/loconotion/update.sh"], capture_output=True)
+
+    content = "Website refreshed successfully." if response.returncode == 0 else f"Website refresh failed with exit code {response.returncode}. Check system log for details."
+    await inter.edit_original_message(content=content)
+    
+    print(content)
+    if response.returncode != 0:
+        print('website refresh stdout:', response.stdout.decode())
+        print('website refresh stderr:', response.stderr.decode())
     sys.stdout.flush()
 
 bot.run(TOKEN)
