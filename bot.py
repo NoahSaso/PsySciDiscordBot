323fae3c4828f647c21868e768692ac70ec0ae40
Noah Saso <noahsaso@gmail.com>
2022-01-18 12:27:03 -0800

    try id instead of name

--- bot.py
+++ bot.py
@@ -125,7 +125,7 @@ async def birthday(inter, birthday):
         )
     birthday = birthday.strip()
 
-    user = inter.author.name
+    user = inter.author.id
 
     print_flush(f"Saved birthday for {user}: {birthday}")
     await inter.response.send_message(f"Saved {birthday} for {user}!")
