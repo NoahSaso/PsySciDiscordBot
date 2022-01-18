8ebb7e4ece9457c22c1931cec810f58f02e07380
Noah Saso <noahsaso@gmail.com>
2022-01-18 12:32:19 -0800

    pickle in current dir

--- config.py
+++ config.py
@@ -10,5 +10,7 @@ RULES_MESSAGE_ID = int(os.getenv("RULES_MESSAGE_ID"))
 REACTION_EMOJI = os.getenv("REACTION_EMOJI")
 ROLE_ID = int(os.getenv("ROLE_ID"))
 
-BIRTHDAY_PICKLE_FILE = os.getenv("BIRTHDAY_PICKLE_FILE", "birthday.p")
+BIRTHDAY_PICKLE_FILE = os.path.join(
+    os.path.dirname(__file__), os.getenv("BIRTHDAY_PICKLE_FILE", "birthday.p")
+)
 BIRTHDAY_REGEX = re.compile(r"^\d{2}\/\d{2}\/\d{4}$")
